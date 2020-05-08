ce2987e7932a986805063721969fd82898bd4dd9
Oscar <elpulgo@users.noreply.github.com>
2020-05-08 16:32:59 +0200

    Added subcommand for boards

--- commands/root.go
+++ commands/root.go
@@ -8,13 +8,16 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "hugo",
+	Use:   "trello",
 	Short: "Hugo is a very fast static site generator",
 	Long: `A Fast and Flexible Static Site Generator built with
 				  love by spf13 and friends in Go.
 				  Complete documentation is available at http://hugo.spf13.com`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Inside rootCmd PersistentPreRun with args: %v\n", args)
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
+		fmt.Println("Hugo command")
 	},
 }
 
