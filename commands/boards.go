da72a8672d183aaace9ff7c6e865a1b6439e3305
Oscar <oscar.f.larsson@gmail.com>
2020-05-08 16:32:59 +0200

    Added subcommand for boards

--- commands/boards.go
+++ commands/boards.go
@@ -1,7 +1,20 @@
 package commands
 
-import "fmt"
+import (
+	"fmt"
 
-func Run() {
-	fmt.Println("Board  cmd ..")
+	"github.com/spf13/cobra"
+)
+
+var boardsCommand = &cobra.Command{
+	Use:   "boards",
+	Short: "Show all boards for user",
+	Long:  `Show all boards for the user, in alphabetical order with numeric shortkey`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Boards command. Yippikajay!")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(boardsCommand)
 }
