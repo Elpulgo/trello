e2b69f3c2f7b161d2952855f0aa747049c4d9c1e
Oscar <elpulgo@users.noreply.github.com>
2020-05-13 14:07:07 +0200

    Added flags for credentials command

--- commands/credentials.go
+++ commands/credentials.go
@@ -6,6 +6,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	key             string
+	token           string
+	passphrase      string
+	storepassphrase string
+)
+
 var credentialsCommand = &cobra.Command{
 	Use:   "credentials",
 	Short: "Set credentials for Trello API.",
@@ -16,15 +23,26 @@ Set credentials for Trello API.
 
 Token and key is required for use of the CLI.
 Can be obtained @ https://trello.com/1/appKey/generate.
+
 Protect the API credentials with a password of choice.
 The password can be saved encoded in a file if you don't wan't to be prompted each time.
 (Will be saved in 'pass.dat')
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Credentials command FIRED!")
+		fmt.Println("Credentials command FIRED!" + key + " token: " + token + " pp:" + passphrase)
 	},
 }
 
 func init() {
+
+	credentialsCommand.Flags().StringVarP(&key, "trello-key", "k", "", "(*) Trello API key.")
+	credentialsCommand.Flags().StringVarP(&token, "trello-token", "t", "", "(*) Trello API token.")
+	credentialsCommand.Flags().StringVarP(&passphrase, "passphrase", "p", "", "(*) Passphrase for API credentials.")
+	credentialsCommand.Flags().StringVarP(&storepassphrase, "store", "s", "", "Should store passphrase in 'pass.dat' (y/n)")
+
 	rootCmd.AddCommand(credentialsCommand)
+
+	credentialsCommand.MarkFlagRequired("trello-key")
+	credentialsCommand.MarkFlagRequired("trello-token")
+	credentialsCommand.MarkFlagRequired("passphrase")
 }
