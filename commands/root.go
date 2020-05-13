fab29349e0c54d5099dbddba441587551b7de747
Oscar <oscar.f.larsson@gmail.com>
2020-05-13 13:47:05 +0200

    Show help if root command without subcommand is executed. Some formatting of CLI description

--- commands/root.go
+++ commands/root.go
@@ -11,26 +11,29 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "tre",
 	Short: "A simple CLI for Trello boards.",
-	Long: `A simple CLI for Trello boards and tasks. 
-	Built in Go for simplicity. Requires an API Key and Token for your Trello board.
-	These will be stored in encrypted files on disk, protected by a password of choice.
-	Optional to store the password on disk aswell if you don't want to be prompted on each use.
-	Complete documentation is available at http://github.com/elpulgo/trello`,
+	Long: `
+A simple CLI for Trello boards and tasks. 
+
+===========================================================================================
+
+Built in Go for simplicity. Requires an API Key and Token for your Trello board.
+These will be stored in encrypted files on disk, protected by a password of choice.
+Optional to store the password on disk aswell if you don't want to be prompted on each use.
+Complete documentation is available at http://github.com/elpulgo/trello`,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+
+		if cmd.Name() == "tre" {
+			return
+		}
 
 		credentialsExists, key, token := credentials.GetCredentials()
 
 		if !credentialsExists {
 			createCredentials()
 		}
-
-		fmt.Println("Key:" + key)
-		fmt.Println("Token:" + token)
-
-		fmt.Printf("Inside rootCmd PersistentPreRun with args: %v\n", args)
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hugo command")
+		cmd.Help()
 	},
 }
 
