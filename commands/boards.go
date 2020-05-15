8925571f8601853ae7b929a3ae01ec496940996b
Oscar <oscar.f.larsson@gmail.com>
2020-05-15 11:04:11 +0200

    Fetching actions from a specific board id.

--- commands/boards.go
+++ commands/boards.go
@@ -24,10 +24,10 @@ var boardsCommand = &cobra.Command{
 	Short: "Show all boards for user",
 	Long:  `Show all boards for the user, in alphabetical order with numeric shortkey`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if specificBoard == "" {
+		if specificBoard == "" && boardName == "" {
 			printBoards()
 		} else {
-			printSpecificBoard()
+			printCards()
 		}
 	},
 }
@@ -39,33 +39,8 @@ func init() {
 	rootCmd.AddCommand(boardsCommand)
 }
 
-func printSpecificBoard() {
-	if len(specificBoard) > 2 {
-		fmt.Println("Printed id")
-	} else {
-		fmt.Println("Printed #")
-	}
-
-	fmt.Println("Specific board: " + specificBoard)
-}
-
 func printBoards() {
-	response, error := http.Get(getUrl())
-	if error != nil {
-		fmt.Println("\n@ Failed to get boards from Trello API. Will exit.")
-		os.Exit(1)
-	}
-
-	defer response.Body.Close()
-
-	body, error := ioutil.ReadAll(response.Body)
-	if error != nil {
-		fmt.Println("\n@ Failed to parse boards from Trelo API response. Will exit.")
-		os.Exit(1)
-	}
-
-	var boards []models.Board
-	json.Unmarshal(body, &boards)
+	boards := getAllBoards()
 
 	const padding = 8
 	writer := tabwriter.NewWriter(os.Stdout, 18, 8, padding, '\t', tabwriter.AlignRight)
@@ -79,7 +54,80 @@ func printBoards() {
 	}
 }
 
-func getUrl() string {
+func printCards() {
+	var actions []models.Action
+
+	if len(specificBoard) > 2 {
+		actions = getCards(specificBoard)
+	} else {
+		fmt.Println("Need to collect all boards first and filter")
+		// boards := getAllBoards()
+	}
+
+	output := make(map[string][]models.Action)
+
+	for _, m := range actions {
+		if m.Data.List.Name == "" {
+			continue
+		}
+
+		output[m.Data.List.Name] = append(output[m.Data.List.Name], m)
+	}
+
+	for list, actions := range output {
+		fmt.Println(" - " + list)
+		for _, action := range actions {
+			fmt.Println("\t" + action.Data.Card.Name)
+			if action.Data.Text != "" {
+				fmt.Println("\t\t" + action.Data.Text)
+			}
+		}
+		fmt.Println("")
+	}
+}
+
+func getAllBoards() []models.Board {
+	response, error := http.Get(getAllBoardsUrl())
+	if error != nil {
+		fmt.Println("\n@ Failed to get boards from Trello API. Will exit.")
+		os.Exit(1)
+	}
+
+	defer response.Body.Close()
+
+	body, error := ioutil.ReadAll(response.Body)
+	if error != nil {
+		fmt.Println("\n@ Failed to parse boards from Trello API response. Will exit.")
+		os.Exit(1)
+	}
+
+	var boards []models.Board
+	json.Unmarshal(body, &boards)
+	return boards
+}
+
+func getCards(boardId string) []models.Action {
+	response, error := http.Get(getCardsUrl(boardId))
+
+	if error != nil {
+		fmt.Println("\n@ Failed to get cards from Trello API. Will exit.")
+		os.Exit(1)
+	}
+
+	defer response.Body.Close()
+
+	body, error := ioutil.ReadAll(response.Body)
+	if error != nil {
+		fmt.Println("\n@ Failed to parse cards from Trello API response. Will exit.")
+		os.Exit(1)
+	}
+
+	var actions []models.Action
+	json.Unmarshal(body, &actions)
+	return actions
+}
+
+func getAllBoardsUrl() string {
 	success, key, token := credentialsmanager.GetCredentials()
 
 	if !success {
@@ -87,4 +135,14 @@ func getUrl() string {
 	}
 
 	return "https://api.trello.com/1/members/me/boards?key=" + key + "&token=" + token
+}
+
+func getCardsUrl(boardId string) string {
+	success, key, token := credentialsmanager.GetCredentials()
+
+	if !success {
+		os.Exit(1)
+	}
+
+	return "https://api.trello.com/1/boards/" + boardId + "/actions?key=" + key + "&token=" + token
 }
