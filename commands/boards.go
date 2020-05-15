736b4006d1a37b4afb284dd020e604efb0b1f7fb
Oscar <oscar.f.larsson@gmail.com>
2020-05-15 16:20:49 +0200

    Can show lists for specific board if flagged with id

--- commands/boards.go
+++ commands/boards.go
@@ -17,6 +17,9 @@ import (
 var (
 	specificBoard string
 	boardName     string
+	trelloKey     string
+	trellotoken   string
+	success       bool
 )
 
 var boardsCommand = &cobra.Command{
@@ -24,6 +27,12 @@ var boardsCommand = &cobra.Command{
 	Short: "Show all boards for user",
 	Long:  `Show all boards for the user, in alphabetical order with numeric shortkey`,
 	Run: func(cmd *cobra.Command, args []string) {
+		success, trelloKey, trellotoken = credentialsmanager.GetCredentials()
+
+		if !success {
+			os.Exit(1)
+		}
+
 		if specificBoard == "" && boardName == "" {
 			printBoards()
 		} else {
@@ -56,32 +65,47 @@ func printBoards() {
 
 func printCards() {
 	var actions []models.Action
+	var lists []models.List
 
 	if len(specificBoard) > 2 {
 		actions = getCards(specificBoard)
+		lists = getLists(specificBoard)
 	} else {
 		fmt.Println("Need to collect all boards first and filter")
 		// boards := getAllBoards()
 	}
 
+	// TODO: Get cards instead of actions.. still sort them on lists.
+	// And then get comments for each card. Use async here to not block or do in paralell?
 	output := make(map[string][]models.Action)
+	listMap := make(map[string]string)
 
-	for _, m := range actions {
-		if m.Data.List.Name == "" {
+	for _, m := range lists {
+		if m.Id == "" {
 			continue
 		}
 
-		output[m.Data.List.Name] = append(output[m.Data.List.Name], m)
+		listMap[m.Id] = m.Name
+		output[m.Id] = append(output[m.Id], models.Action{})
 	}
 
-	for list, actions := range output {
-		fmt.Println(" - " + list)
+	for _, action := range actions {
+		output[action.ListId] = append(output[action.ListId], action)
+	}
+
+	for listId, actions := range output {
+		fmt.Println("-" + listMap[listId])
+
 		for _, action := range actions {
-			fmt.Println("\t" + action.Data.Card.Name)
-			if action.Data.Text != "" {
-				fmt.Println("\t\t" + action.Data.Text)
+			if action.Name == "" {
+				continue
 			}
+
+			fmt.Println("\t# " + action.Name)
 		}
+		// if action.Data.Text != "" {
+		// 	fmt.Println("\t\t" + action.Data.Text)
+		// }
 		fmt.Println("")
 	}
 }
@@ -124,25 +148,40 @@ func getCards(boardId string) []models.Action {
 
 	var actions []models.Action
 	json.Unmarshal(body, &actions)
+
 	return actions
 }
 
-func getAllBoardsUrl() string {
-	success, key, token := credentialsmanager.GetCredentials()
+func getLists(boardId string) []models.List {
+	response, error := http.Get(getListsUrl(boardId))
 
-	if !success {
+	if error != nil {
+		fmt.Println("\n@ Failed to get cards from Trello API. Will exit.")
 		os.Exit(1)
 	}
 
-	return "https://api.trello.com/1/members/me/boards?key=" + key + "&token=" + token
+	defer response.Body.Close()
+
+	body, error := ioutil.ReadAll(response.Body)
+	if error != nil {
+		fmt.Println("\n@ Failed to parse cards from Trello API response. Will exit.")
+		os.Exit(1)
+	}
+
+	var lists []models.List
+	json.Unmarshal(body, &lists)
+
+	return lists
+}
+
+func getAllBoardsUrl() string {
+	return "https://api.trello.com/1/members/me/boards?key=" + trelloKey + "&token=" + trellotoken
 }
 
 func getCardsUrl(boardId string) string {
-	success, key, token := credentialsmanager.GetCredentials()
+	return "https://api.trello.com/1/boards/" + boardId + "/cards?key=" + trelloKey + "&token=" + trellotoken
+}
 
-	if !success {
-		os.Exit(1)
-	}
-
-	return "https://api.trello.com/1/boards/" + boardId + "/actions?key=" + key + "&token=" + token
+func getListsUrl(boardId string) string {
+	return "https://api.trello.com/1/boards/" + boardId + "/lists?key=" + trelloKey + "&token=" + trellotoken
 }
