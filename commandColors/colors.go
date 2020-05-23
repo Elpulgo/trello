538cacc78f994de9fe79c99f83fc5db638c834f3
Oscar <elpulgo@users.noreply.github.com>
2020-05-23 10:09:37 +0200

    Added extended colors func

---
+++ commandColors/colors.go
@@ -0,0 +1,37 @@
+package commandColors
+
+import (
+	"github.com/gookit/color"
+)
+
+func Red(value string) string {
+	return color.FgRed.Render(value)
+}
+
+func RedBold(value string) string {
+	return color.Style{color.FgRed, color.OpBold}.Render(value)
+}
+
+func Green(value string) string {
+	return color.FgGreen.Render(value)
+}
+
+func GreenBold(value string) string {
+	return color.Style{color.FgGreen, color.OpBold}.Render(value)
+}
+
+func Yellow(value string) string {
+	return color.FgYellow.Render(value)
+
+}
+func YellowBold(value string) string {
+	return color.Style{color.FgYellow, color.OpBold}.Render(value)
+}
+
+func Cyan(value string) string {
+	return color.FgCyan.Render(value)
+
+}
+func CyanBold(value string) string {
+	return color.Style{color.FgCyan, color.OpBold}.Render(value)
+}
