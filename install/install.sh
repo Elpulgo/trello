a01539f8d82184f3b0df8ccd14d5c3e1a640ab37
Oscar <oscar.f.larsson@gmail.com>
2020-07-01 14:21:15 +0200

    Updated script and removed f flag when unzip tar

--- install/install.sh
+++ install/install.sh
@@ -2,7 +2,7 @@
 
 install() {
     export FILE="tre"
-    curl "https://raw.githubusercontent.com/Elpulgo/trello/master/dist/tre.tar.gz" | tar xvzf
+    curl "https://raw.githubusercontent.com/Elpulgo/trello/master/dist/tre.tar.gz" | tar xvz
     chmod +x tre
     sudo mv tre /usr/local/bin/tre
 }
